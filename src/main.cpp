299c405407d5bc273eaf3f42255f63edf7260497
orchnp <egorgrigoriev124@gmail.com>
2026-09-02 18:09:06 +0600

    feat: init verticies and vertexShader

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 int main() {
   // glfw initialization
   glfwInit();
@@ -11,13 +19,16 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // creating glfw window
-  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  GLFWwindow *window =
+      glfwCreateWindow(WIDTH, HEIHGT, "LearnOpenGL", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
   }
   glfwMakeContextCurrent(window);
+  // init OpenGl viewport and handle resize
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   // initialize glad for OpenGL function pointers(OS-specific)
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -25,9 +36,38 @@ int main() {
     return -1;
   }
 
-  // init OpenGl viewport and handle resize
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // vertexShader init and bind
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  // shader errors handle
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  // vertices and VBO init
+  // clang-format off
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f, 
+    0.5f, -0.5f, 0.0f, 
+    0.0f, 0.5f, 0.0f
+  };
+  // clnag-format on
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   // render loop
   while (!glfwWindowShouldClose(window)) {
--- src/main.hpp
+++ src/main.hpp
@@ -2,6 +2,9 @@
 
 #include <GLFW/glfw3.h>
 
+#define WIDTH 800
+#define HEIHGT 600
+
 // callback for resize window
 void framebuffer_size_callback(GLFWwindow *, int width, int height);
 
